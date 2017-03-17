d0fad5ac10ec3ac4f7f1cc548ebd57b2882f4bf2
Etienne Millon <etienne@cryptosense.com>
2017-03-17 23:09:18 +0100

    Extract P11_flags

--- src/p11.ml
+++ src/p11.ml
@@ -8,38 +8,7 @@ module Object_handle = P11_object_handle
 module HW_feature_type = P11_hw_feature_type
 module Slot = P11_slot
 module Slot_id = P11_slot_id
-
-module Flags =
-struct
-  include Pkcs11.CK_FLAGS
-
-  let to_json ?pretty (flags:t) =
-    match pretty with
-      | None ->
-        Pkcs11_CK_ULONG.to_yojson flags
-      | Some pretty ->
-          `Assoc [
-            "value", Pkcs11_CK_ULONG.to_yojson flags;
-            "string", `String (pretty flags);
-          ]
-
-  type has_value =
-    { value : Yojson.Safe.json
-    ; string : string
-    }
-  [@@deriving of_yojson]
-
-  let of_yojson json =
-    (* We know that [Pkcs11_CK_ULONG.to_yojson] does not produce [`Assoc]s. *)
-    let actual_json = match has_value_of_yojson json with
-      | Ok { value } -> value
-      | Error _ -> json
-    in
-    Pkcs11_CK_ULONG.of_yojson actual_json
-
-  let to_yojson =
-    to_json ?pretty:None
-end
+module Flags = P11_flags
 
 module Object_class =
 struct
--- src/p11.mli
+++ src/p11.mli
@@ -15,73 +15,7 @@ module Object_handle = P11_object_handle
 module HW_feature_type = P11_hw_feature_type
 module Slot = P11_slot
 module Slot_id = P11_slot_id
-
-module Flags :
-sig
-  type t = Pkcs11.CK_FLAGS.t
-  [@@deriving yojson]
-  val empty : t
-  val compare : t -> t -> int
-  val equal : t -> t -> bool
-  val logical_or : t -> t -> t
-  val ( || ) : t -> t -> t
-  val get : flags: t -> flag: t -> bool
-  val _CKF_TOKEN_PRESENT : t
-  val _CKF_REMOVABLE_DEVICE : t
-  val _CKF_HW_SLOT : t
-  val _CKF_RNG : t
-  val _CKF_WRITE_PROTECTED : t
-  val _CKF_LOGIN_REQUIRED : t
-  val _CKF_USER_PIN_INITIALIZED : t
-  val _CKF_RESTORE_KEY_NOT_NEEDED : t
-  val _CKF_CLOCK_ON_TOKEN : t
-  val _CKF_PROTECTED_AUTHENTICATION_PATH : t
-  val _CKF_DUAL_CRYPTO_OPERATIONS : t
-  val _CKF_TOKEN_INITIALIZED : t
-  val _CKF_SECONDARY_AUTHENTICATION : t
-  val _CKF_USER_PIN_COUNT_LOW : t
-  val _CKF_USER_PIN_FINAL_TRY : t
-  val _CKF_USER_PIN_LOCKED : t
-  val _CKF_USER_PIN_TO_BE_CHANGED : t
-  val _CKF_SO_PIN_COUNT_LOW : t
-  val _CKF_SO_PIN_FINAL_TRY : t
-  val _CKF_SO_PIN_LOCKED : t
-  val _CKF_SO_PIN_TO_BE_CHANGED : t
-  val _CKF_RW_SESSION : t
-  val _CKF_SERIAL_SESSION : t
-  val _CKF_ARRAY_ATTRIBUTE : t
-  val _CKF_HW : t
-  val _CKF_ENCRYPT : t
-  val _CKF_DECRYPT : t
-  val _CKF_DIGEST : t
-  val _CKF_SIGN : t
-  val _CKF_SIGN_RECOVER : t
-  val _CKF_VERIFY : t
-  val _CKF_VERIFY_RECOVER : t
-  val _CKF_GENERATE : t
-  val _CKF_GENERATE_KEY_PAIR : t
-  val _CKF_WRAP : t
-  val _CKF_UNWRAP : t
-  val _CKF_DERIVE : t
-  val _CKF_EC_F_P : t
-  val _CKF_EC_F_2M : t
-  val _CKF_EC_ECPARAMETERS : t
-  val _CKF_EC_NAMEDCURVE : t
-  val _CKF_EC_UNCOMPRESS : t
-  val _CKF_EC_COMPRESS : t
-  val _CKF_EXTENSION : t
-  val _CKF_LIBRARY_CANT_CREATE_OS_THREADS : t
-  val _CKF_OS_LOCKING_OK : t
-  val _CKF_DONT_BLOCK : t
-  val _CKF_NEXT_OTP : t
-  val _CKF_EXCLUDE_TIME : t
-  val _CKF_EXCLUDE_COUNTER : t
-  val _CKF_EXCLUDE_CHALLENGE : t
-  val _CKF_EXCLUDE_PIN : t
-  val _CKF_USER_FRIENDLY_OTP : t
-  val to_string : t -> string
-  val of_string : string -> t
-end
+module Flags = P11_flags
 
 module Object_class :
 sig
