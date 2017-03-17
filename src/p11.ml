abb31a054d71c369c781614aed28a2b24a515146
Etienne Millon <etienne@cryptosense.com>
2017-03-18 00:10:35 +0100

    Extract P11_token_info

--- src/p11.ml
+++ src/p11.ml
@@ -28,61 +28,7 @@ module RAW_PAYLOAD_params = P11_raw_payload_params
 module Mechanism = P11_mechanism
 module User_type = P11_user_type
 module Info = P11_info
-
-module Token_info =
-struct
-  type t = Pkcs11.CK_TOKEN_INFO.u =
-    {
-      label : string;
-      manufacturerID : string;
-      model : string;
-      serialNumber : string;
-      flags : Flags.t;
-      ulMaxSessionCount : Pkcs11_CK_ULONG.t;
-      ulSessionCount : Pkcs11_CK_ULONG.t;
-      ulMaxRwSessionCount : Pkcs11_CK_ULONG.t;
-      ulRwSessionCount : Pkcs11_CK_ULONG.t;
-      ulMaxPinLen : Pkcs11_CK_ULONG.t;
-      ulMinPinLen : Pkcs11_CK_ULONG.t;
-      ulTotalPublicMemory : Pkcs11_CK_ULONG.t;
-      ulFreePublicMemory : Pkcs11_CK_ULONG.t;
-      ulTotalPrivateMemory : Pkcs11_CK_ULONG.t;
-      ulFreePrivateMemory : Pkcs11_CK_ULONG.t;
-      hardwareVersion : Version.t;
-      firmwareVersion : Version.t;
-      utcTime : string;
-    }
-    [@@deriving of_yojson]
-
-  let ul_to_string = Pkcs11.CK_TOKEN_INFO.ul_to_string
-  let to_string = Pkcs11.CK_TOKEN_INFO.to_string
-  let to_strings = Pkcs11.CK_TOKEN_INFO.to_strings
-  let flags_to_string = Pkcs11.CK_TOKEN_INFO.string_of_flags
-
-  let to_yojson info =
-    let ulong x = `String (Unsigned.ULong.to_string x) in
-    `Assoc [
-      "label", `String info.label;
-      "manufacturerID", `String info.manufacturerID;
-      "model", `String info.model;
-      "serialNumber", `String info.serialNumber;
-      "flags",
-      Flags.to_json ~pretty:flags_to_string info.flags;
-      "ulMaxSessionCount", ulong info.ulMaxSessionCount;
-      "ulSessionCount", ulong info.ulSessionCount;
-      "ulMaxRwSessionCount", ulong info.ulMaxRwSessionCount;
-      "ulRwSessionCount", ulong info.ulRwSessionCount;
-      "ulMaxPinLen", ulong info.ulMaxPinLen;
-      "ulMinPinLen", ulong info.ulMinPinLen;
-      "ulTotalPublicMemory", ulong info.ulTotalPublicMemory;
-      "ulFreePublicMemory", ulong info.ulFreePublicMemory;
-      "ulTotalPrivateMemory", ulong info.ulTotalPrivateMemory;
-      "ulFreePrivateMemory", ulong info.ulFreePrivateMemory;
-      "hardwareVersion", Version.to_yojson info.hardwareVersion;
-      "firmwareVersion", Version.to_yojson info.firmwareVersion;
-      "utcTime", `String info.utcTime;
-    ]
-end
+module Token_info = P11_token_info
 
 module Slot_info =
 struct
--- src/p11.mli
+++ src/p11.mli
@@ -26,36 +26,7 @@ module RAW_PAYLOAD_params = P11_raw_payload_params
 module Mechanism = P11_mechanism
 module User_type = P11_user_type
 module Info = P11_info
-
-module Token_info :
-sig
-  type t = Pkcs11.CK_TOKEN_INFO.u =
-    {
-      label : string;
-      manufacturerID : string;
-      model : string;
-      serialNumber : string;
-      flags : Flags.t;
-      ulMaxSessionCount : Unsigned.ULong.t;
-      ulSessionCount : Unsigned.ULong.t;
-      ulMaxRwSessionCount : Unsigned.ULong.t;
-      ulRwSessionCount : Unsigned.ULong.t;
-      ulMaxPinLen : Unsigned.ULong.t;
-      ulMinPinLen : Unsigned.ULong.t;
-      ulTotalPublicMemory : Unsigned.ULong.t;
-      ulFreePublicMemory : Unsigned.ULong.t;
-      ulTotalPrivateMemory : Unsigned.ULong.t;
-      ulFreePrivateMemory : Unsigned.ULong.t;
-      hardwareVersion : Version.t;
-      firmwareVersion : Version.t;
-      utcTime : string;
-    }
-  [@@deriving yojson]
-  val ul_to_string : Unsigned.ULong.t -> string
-  val to_string : ?newlines: bool -> ?indent: string -> t -> string
-  val to_strings : t -> string list
-  val flags_to_string : Flags.t -> string
-end
+module Token_info = P11_token_info
 
 module Slot_info :
 sig
