835d317d9f0b5a3d9db52bb72a0f6765daeb1d36
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2017-05-11 14:33:23 +0200

    Reverse dependencies: P11_slot_id

--- src/p11_slot_id.ml
+++ src/p11_slot_id.ml
@@ -1,5 +1,20 @@
-type t = Pkcs11.CK_SLOT_ID.t
-[@@deriving eq,ord,show,yojson]
+type t = Unsigned.ULong.t
+[@@deriving ord]
+
+let equal a b =
+  compare a b = 0
+
+let show = Unsigned.ULong.to_string
+
+let pp fmt n =
+  Format.pp_print_string fmt (show n)
+
+let of_yojson = function
+  | `String s -> Ok (Unsigned.ULong.of_string s)
+  | _ -> Error "ulong_of_yojson: not a string"
+
+let to_yojson ulong =
+  `String (Unsigned.ULong.to_string ulong)
 
 let to_string = Unsigned.ULong.to_string
 let hash  = Unsigned.ULong.to_int
