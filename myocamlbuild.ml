85de825423291c6b4f89b3dbff3cb995ef6617d4
Etienne Millon <etienne@cryptosense.com>
2017-05-29 13:30:22 +0200

    Move reverse bindings to src_rev

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -12,11 +12,11 @@ let ctypes_generator name base : unit =
        Cmd (S [P exe; A arg])
     )
 
-let lib base =
+let lib ~dir base =
   let use_flag = "use_" ^ base in
   let link_flag = "link_" ^ base in
   let lib_installed = Printf.sprintf "-l%s_stubs" base in
-  let lib_build = Printf.sprintf "src/lib%s_stubs.a" base in
+  let lib_build = Printf.sprintf "%s/lib%s_stubs.a" dir base in
   flag ["library"; "link"; "byte"; use_flag] (S ([A "-dllib"; A lib_installed]));
   flag ["library"; "link"; "native"; use_flag] (S ([A "-cclib"; A lib_installed]));
   flag ["link"; "ocaml"; link_flag] (A lib_build);
@@ -26,10 +26,10 @@ let after_rules () =
   ctypes_generator "from **/*_generator.ml" "%(name: <**/*> and not <**/*_generator>)";
   let ocaml_ctypes_lib_path = Findlib.((query "ctypes").location) in
   flag ["compile"; "c"] (S [A "-I"; P ocaml_ctypes_lib_path]);
-  pdep ["compile"; "c"] "depend" (fun s -> [s]);
+  pdep ["compile"] "depend" (fun s -> [s]);
   pflag ["compile";"c"] "depend" (fun s -> S [A "-I"; P (Filename.dirname s)]);
-  lib "pkcs11";
-  lib "pkcs11_rev";
+  lib ~dir:"src" "pkcs11";
+  lib ~dir:"src_rev" "pkcs11_rev";
   flag ["here"] (S ([A "-cclib"; A "-Lsrc"]));
   ()
 
