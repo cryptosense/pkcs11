94e23ba2c1aafa0e2e04c0c0c3ef204699d64198
Etienne Millon <etienne@cryptosense.com>
2017-06-07 17:34:19 +0200

    Remove dependencies to Pkcs11_CK_OBJECT_HANDLE

--- src/p11_ecmqv_derive_params.ml
+++ src/p11_ecmqv_derive_params.ml
@@ -3,9 +3,9 @@ type t =
   ; shared_data: string option
   ; public_data: string
   ; private_data_len: P11_ulong.t
-  ; private_data: Pkcs11_CK_OBJECT_HANDLE.t
+  ; private_data: P11_object_handle.t
   ; public_data2: string
-  ; public_key: Pkcs11_CK_OBJECT_HANDLE.t
+  ; public_key: P11_object_handle.t
   }
 
 let compare : t -> t -> int =
